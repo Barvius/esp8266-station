e3da3e525e6ff0aa755786fcb29b7d4fe77202df
barvius <barabanshikov.oleg@yandex.ru>
2018-07-16 17:48:16 +0300

    dev-0.0.4

--- esp8266-station.ino
+++ esp8266-station.ino
@@ -6,12 +6,14 @@
 #include <Adafruit_BMP085.h>
 
 #define CONFIG_PIN 13
+#define CHARGE_BEGIN_PIN 14
+#define CHARGE_END_PIN 12
 
 Adafruit_BMP085 bmp;
 ESP8266WebServer HTTP(80);
 
 void ConnectWiFi() {
-  
+
   WiFi.begin();
 
   while (WiFi.status() != WL_CONNECTED) {
@@ -48,50 +50,50 @@ void SendMeasure() {
   httpClient.setUserAgent("ESP8266");
 
   String req = "cid=" + String(ESP.getChipId());
-//  Serial.println(bmp.readTemperature());
-//  Serial.println(bmp.readPressure() / 133.3);
   req += "&temperature_" + String(ESP.getChipId()) + "=";
   req += bmp.readTemperature();
   req += "&pressure_" + String(ESP.getChipId()) + "=";
   req += bmp.readPressure() / 133.3;
   req += "&voltage_" + String(ESP.getChipId()) + "=";
-  req += String(5.35*((float)analogRead(A0)/1023));
- 
- 
-  
+  req += String(5.3 * ((float)analogRead(A0) / 1023));
+  req += "&charge_begin_" + String(ESP.getChipId()) + "=";
+  req += String(digitalRead(CHARGE_BEGIN_PIN));
+  req += "&charge_end_" + String(ESP.getChipId()) + "=";
+  req += String(digitalRead(CHARGE_END_PIN));
+
   httpClient.begin("http://192.168.1.141/update_station.php?" + req);
-//  Serial.printf("[HTTP] GET ... code: %d\r\n", httpClient.GET());
-httpClient.GET();
+ 
+  httpClient.GET();
   httpClient.end();
 
-  
+
 }
 
 
 void setup() {
   Serial.begin(115200);
-//  while (bmp.begin()) {
-////    delay(500);
-//    Serial.println("errror");
-//  }
-//  Serial.println(WiFi.psk());
-//  Serial.println(5.275*((float)analogRead(A0)/1023));
-//  pinMode(CONFIG_PIN, INPUT);
-//  if (digitalRead(CONFIG_PIN) == HIGH) {
-//    WiFi.softAP(String("ESP_" + String(ESP.getChipId())).c_str(), String("ESP_" + String(ESP.getChipId())).c_str());
-//    ConfigServer();
-//  } 
-    ConnectWiFi();
   
-    bmp.begin();
-    SendMeasure();
-    UpdateFirmware();
-    ESP.deepSleep(1 * 60 * 1000 * 1000);
-//  }
+  //  pinMode(CONFIG_PIN, INPUT);
+  //  if (digitalRead(CONFIG_PIN) == HIGH) {
+  //    WiFi.softAP(String("ESP_" + String(ESP.getChipId())).c_str(), String("ESP_" + String(ESP.getChipId())).c_str());
+  //    ConfigServer();
+  //  }
+  
+  pinMode(CONFIG_PIN, INPUT);
+  pinMode(CHARGE_BEGIN_PIN, INPUT);
+  pinMode(CHARGE_END_PIN, INPUT);
+  
+  ConnectWiFi();
+
+  bmp.begin();
+  SendMeasure();
+  UpdateFirmware();
+  ESP.deepSleep(5 * 60 * 1000 * 1000);
+  
 
 }
 
 void loop() {
-//  HTTP.handleClient();
-//  delay(1);
+  //  HTTP.handleClient();
+  //delay(1);
 }
